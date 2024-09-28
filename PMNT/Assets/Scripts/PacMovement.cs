1c9c9de80ead0c3170c655b9363a7c7d4c92f72d
SpcaeCharlie <145831065+SpcaeCharlie@users.noreply.github.com>
2024-09-28 13:25:41 +1000

    animations added

--- PMNT/Assets/Scripts/PacMovement.cs
+++ PMNT/Assets/Scripts/PacMovement.cs
@@ -6,11 +6,13 @@ public class PacMovement : MonoBehaviour
 {
     public GameObject pac;
 
+    private Animator pacanim;
+
     float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pacanim = pac.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,20 +21,24 @@ public class PacMovement : MonoBehaviour
         if (pac.transform.position.x >=-3.75f && pac.transform.position.y >4.75f)
         {
             pac.transform.position += Vector3.down * speed * Time.deltaTime;
+            pacanim.Play("PacDown");
         }
         
         if (pac.transform.position.x > -6.19f && pac.transform.position.y <= 4.75f)
         {
             pac.transform.position += Vector3.left * speed * Time.deltaTime;
+            pacanim.Play("PacLeft");
         }
 
         if (pac.transform.position.x <= -6.19 && pac.transform.position.y < 6.69)
         {
             pac.transform.position += Vector3.up * speed * Time.deltaTime;
+            pacanim.Play("PacUp");
         }
         if (pac.transform.position.x < -3.75 && pac.transform.position.y >= 6.69)
         {
            pac.transform.position += Vector3.right * speed * Time.deltaTime;
+            pacanim.Play("PacRight");
         }
 
    
