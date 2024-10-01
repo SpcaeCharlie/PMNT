1a29a116e58be0f241a01bcbf7eaf03e51d0d059
SpcaeCharlie <145831065+SpcaeCharlie@users.noreply.github.com>
2024-10-01 20:18:22 +1000

    corners now correctly rotate
    this is possibly the worst piece of code I have ever devised but it works, I hope.

--- PMNT/Assets/Scripts/LevelGenerator.cs
+++ PMNT/Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
@@ -12,9 +13,9 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(generatorarray == null)
+        if (generatorarray == null)
         {
-            generatorarray = new int[,] 
+            generatorarray = new int[,]
 { {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
 {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
 {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
@@ -29,7 +30,7 @@ public class LevelGenerator : MonoBehaviour
 {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
 {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
 {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
-{0,0,0,0,0,0,5,0,0,0,4,0,0,0}} ;
+{0,0,0,0,0,0,5,0,0,0,4,0,0,0}};
         }
 
 
@@ -41,11 +42,12 @@ public class LevelGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-    void Generator() {
+    void Generator()
+    {
 
         int Levelx = generatorarray.GetLength(1);
         int Levely = generatorarray.GetLength(0);
@@ -57,60 +59,93 @@ public class LevelGenerator : MonoBehaviour
         tile.transform.position = new Vector3(0, 0, 0);
         Debug.Log(Levely);
         Debug.Log(Levelx);
+
+        int test = 0;
+
         for (int i = 0; i < Levelx; i++)
         {
             for (int j = 0; j < Levely; j++)
             {
+
+                bool north= false;
+                bool south = false;
+                bool east = false;
+                bool west = false;
                 int tilenum = generatorarray[j, i];
-                Vector3 rotation;
+                Vector3 rotation= new Vector3(0,0,0);
 
-                if (tilenum == 1 || tilenum == 3 || tilenum ==2 || tilenum == 4 || tilenum == 7)
-                {
-                    if (i - 1 >= 0)
+                    if (tilenum == 1 || tilenum == 3 || tilenum == 2 || tilenum == 4 || tilenum == 7)
                     {
-                        int north = generatorarray[j, i - 1];
-                    }
-                    if (i + 1 < Levelx)
-                    {
-                       int south = generatorarray[j, i+1 ];
-                    }
-                    if (j - 1 >= 0)
-                    {
-                        int west = generatorarray[j - 1, i];
-                    }
-                    if (j + 1 < Levely)
-                    {
-                        int east = generatorarray[j + 1, i];
-                    }
+                    if (i - 1 >= 0 && isrotateable(generatorarray[j, i -1])) { north = true;}
+                    if (i + 1 < Levelx && isrotateable(generatorarray[j,i + 1])) { south= true; }
+                    if (j - 1 >= 0 && isrotateable(generatorarray[j - 1,i])) {west = true;}
+                    if (j + 1 < Levely && isrotateable(generatorarray[j +1, i])){east= true; }
                 }
-                if (tilenum ==1 || tilenum == 3)
-                {
+                    if (tilenum == 1 || tilenum == 3)
+                    {
+                        if (south == true && east == true && north == false && west == false)
+                    {
+                        rotation = new Vector3(0,0,0);
+                    }
+                    else if(east == true && north == true && west == false && south == false)
+                    {
+                        rotation = new Vector3(0, 0, 270);
+                    }
+                    else if(north == true && west == true && south == false && east == false)
+                    {
+                        rotation = new Vector3(0, 0, 180);
+                    }
+                    else if(west == true && south == true && east == false && north == false)
+                    {
+                        rotation = new Vector3(0, 0, 90);
+                    }
 
+
+                    else if (south == true && east == true && isrotateable(generatorarray[j+1,i+1]) == false ) 
+                        {
+                        rotation = new Vector3(0, 0, 0);
+                        }
+                        else if (east == true && north == true && isrotateable(generatorarray[j+1, i-1]) == false)
+                        {
+                        rotation = new Vector3(0, 0, 270);
+                        }
+                        else if (north == true && west == true && isrotateable(generatorarray[j-1,i-1]) == false)
+                        {
+                        rotation = new Vector3(0, 0, 180);
+                        }
+                        else if (west == true && south == true && isrotateable(generatorarray[j-1, i+1]) == false)
+                        {
+                        rotation = new Vector3(0, 0, 90);
+                        test += 1;
+                        }
                 }
 
-                
 
-                GameObject temp = Instantiate(tile, new Vector3(0.5f*i, -0.5f*j, 0), Quaternion.identity, generatedlevel.transform);
-                temp.GetComponent<SpriteRenderer>().sprite = sprites[tilenum];
-            }
+
+                GameObject temp = Instantiate(tile, new Vector3(0.5f * i, -0.5f * j, 0), Quaternion.Euler(rotation), generatedlevel.transform);
+                    temp.GetComponent<SpriteRenderer>().sprite = sprites[tilenum];
+              }
         }
-    
-    
 
 
 
-        
+
+
+
+        Debug.Log(test);
+
     }
-
-
-void removecurrentlevel()
+    void removecurrentlevel()
         {
 
             Destroy(GameObject.Find("Level"));
         }
 
 
-
+    bool isrotateable(int tiletype)
+    {
+        return tiletype == 1 || tiletype == 2 || tiletype == 3 || tiletype == 4 || tiletype == 7;
+    }
 
 
 }
