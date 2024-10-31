aff27a55fc044f5ae17c8ce47f6d3e74b9ab1a88
SpcaeCharlie <145831065+SpcaeCharlie@users.noreply.github.com>
2024-10-31 23:52:15 +1100

    added last input variable

--- PMNT/Assets/Scripts/PacStudentController.cs
+++ PMNT/Assets/Scripts/PacStudentController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
+
+    private Vector2 lastInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.w))
+        {
+
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
