aaf452886671b192bb29553ba62dd0535cdce74a
SpcaeCharlie <145831065+SpcaeCharlie@users.noreply.github.com>
2024-11-01 11:38:52 +1100

    PACMAN MOVES!!

--- PMNT/Assets/Scripts/PacStudentController.cs
+++ PMNT/Assets/Scripts/PacStudentController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
 
-    private Vector2 lastInput;
+    private Vector3 lastInput;
+    public Tweener tweener;
+    private Vector3 currentInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +18,46 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.w))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-
+            lastInput = new Vector3(0f, 0.5f,0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            lastInput = new Vector3(0f, -0.5f,0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lastInput = new Vector3(0.5f, 0f,0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lastInput = new Vector3(-0.5f, 0f,0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!tweener.TweenExists(transform))
         {
-
-            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector2(0, 0), 0.2f);
-            foreach (var hitCollider in hitColliders)
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position+lastInput, 0.2f);
+            if (hitColliders[0].gameObject.tag == "Walkable")
             {
-                Debug.Log("Found object: " + hitCollider.gameObject.transform.position);
+                currentInput = lastInput;
+                Vector3 endpos = new Vector3(hitColliders[0].gameObject.transform.position.x, hitColliders[0].gameObject.transform.position.y, -1f);
+                tweener.AddTween(transform, transform.position, endpos, 0.5f);
+            }
+            if (hitColliders[0].gameObject.tag != "Walkable")
+            {
+                Collider2D[] hitColliders2 = Physics2D.OverlapCircleAll(transform.position + currentInput, 0.2f);
+                if (hitColliders2[0].gameObject.tag == "Walkable")
+                {
+
+                    Vector3 endpos2 = new Vector3(hitColliders2[0].gameObject.transform.position.x, hitColliders2[0].gameObject.transform.position.y, -1f);
+
+                    tweener.AddTween(transform, transform.position, endpos2, 0.5f);
+
+                }
             }
         }
+
+       
     }
 }
