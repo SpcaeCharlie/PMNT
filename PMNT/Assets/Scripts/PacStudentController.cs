62c88d147636a2fb37aa3718e040fc21b3a71fb0
SpcaeCharlie <145831065+SpcaeCharlie@users.noreply.github.com>
2024-11-01 14:04:04 +1100

    pacman animates

--- PMNT/Assets/Scripts/PacStudentController.cs
+++ PMNT/Assets/Scripts/PacStudentController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
 
-    private Vector3 lastInput;
+    private int lastInput;
     public Tweener tweener;
-    private Vector3 currentInput;
+    private int currentInput;
+    private List<Vector3> directions = new List<Vector3>(); // Use List instead of array
+    public Animator animator; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        directions.Add(new Vector3(0f, 0.5f, 0));
+        directions.Add(new Vector3(0f, -0.5f, 0));
+        directions.Add(new Vector3(0.5f, 0f, 0));
+        directions.Add(new Vector3(-0.5f, 0f, 0));
     }
 
     // Update is called once per frame
@@ -20,40 +26,41 @@ public class PacStudentController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            lastInput = new Vector3(0f, 0.5f,0);
+            lastInput = 0;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            lastInput = new Vector3(0f, -0.5f,0);
+            lastInput = 1;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            lastInput = new Vector3(0.5f, 0f,0);
+            lastInput = 2;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            lastInput = new Vector3(-0.5f, 0f,0);
+            lastInput = 3;
         }
 
         if (!tweener.TweenExists(transform))
         {
-            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position+lastInput, 0.2f);
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position+directions[lastInput], 0.2f);
             if (hitColliders[0].gameObject.tag == "Walkable")
             {
                 currentInput = lastInput;
                 Vector3 endpos = new Vector3(hitColliders[0].gameObject.transform.position.x, hitColliders[0].gameObject.transform.position.y, -1f);
                 tweener.AddTween(transform, transform.position, endpos, 0.5f);
+                animator.SetInteger("Direction", lastInput);
             }
             if (hitColliders[0].gameObject.tag != "Walkable")
             {
-                Collider2D[] hitColliders2 = Physics2D.OverlapCircleAll(transform.position + currentInput, 0.2f);
+                Collider2D[] hitColliders2 = Physics2D.OverlapCircleAll(transform.position + directions[currentInput], 0.2f);
                 if (hitColliders2[0].gameObject.tag == "Walkable")
                 {
 
                     Vector3 endpos2 = new Vector3(hitColliders2[0].gameObject.transform.position.x, hitColliders2[0].gameObject.transform.position.y, -1f);
 
                     tweener.AddTween(transform, transform.position, endpos2, 0.5f);
-
+                    animator.SetInteger("Direction", currentInput);
                 }
             }
         }
